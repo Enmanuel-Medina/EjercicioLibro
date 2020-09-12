e89d3fef727744686bca8e773d2cd2f3ec8668d3
Jose Enmanuel Jaquez Medina <jose_jaquez1@ucne.edu.do>
2020-09-11 21:22:51 -0400

    Add project files.

---
+++ Ejecicio_Libros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejecicio_Libros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Ejecicio_Libros/Tarea1/Ejercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejecicio_Libros.Tarea1
+{
+     public  class Ejercicio1
+    {
+        public void agregar()
+        {
+            Console.WriteLine("Jose Enmanuel Jaquez Medina");
+
+            Console.WriteLine(" Ejercicio5 Mas nombre");
+
+            Console.WriteLine("Me gusta Esta materia");
+            Console.WriteLine("2017-0485");
+            Console.WriteLine("Espero aprender mucho en esta materia");
+
+        }  
+    }
+}
